fadec1803e3bfd48a069af312658cae58b349fcd
maskman1543 <103424060+maskman1543@users.noreply.github.com>
2024-02-27 12:53:40 +0800

    Camus_updates

--- StudentHub/urls.py
+++ StudentHub/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from authentication import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='root_home'), 
     path('', include('authentication.urls')),
 ]
