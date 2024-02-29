e7dfcd48e288d956f7af16a252c15448711e297f
Yuri <bobtani2234@gmail.com>
2024-02-29 10:45:51 +0800

    updated

---
+++ StudentHub/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.office365.com'
+EMAIL_HOST_USER = 'studenthub52@outlook.com'
+EMAIL_HOST_PASSWORD = 'bmjovfvrwhzfypor'
+EMAIL_PORT = 587
---
+++ authentication/views.py
@@ -0,0 +1,86 @@
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+
+
+# Create your views here.
+def home(request):
+    return render(request, "authentication/index.html")
+
+def signup(request):
+    counter = 0
+
+    if request.method == "POST":
+        counter = 0
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        
+
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exist")
+            counter += 1
+            return redirect('signup')
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already used by another user")
+            counter += 1
+            return redirect('signup')
+        if len(username)>10:
+            messages.error(request, "Username must be under 10 characters")
+            counter += 1
+            return redirect('signup')
+        if password1 != password2:
+            messages.error(request, "Passwords do not match")
+            counter += 1
+            return redirect('signup')
+        if not username.isalnum():
+            messages.error(request, "Username must be alphanumeric")
+            counter += 1
+            return redirect('signup')
+        
+        if counter == 0:
+        
+            myuser = User.objects.create_user(username, email, password1)
+            myuser.first_name = fname
+            myuser.last_name =lname
+
+            myuser.save()
+            messages.success(request, "Your Account has been successfully created.")
+        
+            return redirect('signin')
+        else:
+            return redirect('signup')
+    
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password1 = request.POST['password1']
+
+        user = authenticate(username=username, password=password1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "authentication/index.html", {'fname': fname})
+
+        else:
+            messages.error(request, "Bad Credentials")
+            return redirect('home')
+
+
+    return render(request, "authentication/signin.html")
+
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged Out Successfully!")
+    return redirect('home')
