240f705a3bc7e240aeec4388ee59bdbe7c057836
maskman1543 <103424060+maskman1543@users.noreply.github.com>
2024-02-29 12:10:48 +0800

    Camus update added navbar component

--- StudentHub/settings.py
+++ StudentHub/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'StudentHub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,9 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Additional locations of static files
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
